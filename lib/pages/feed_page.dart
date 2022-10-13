4d145ff92e7ca946d0c8bf8f3850cd5693552539
macbook <macbook@appurunoMacBook-Pro.local>
2022-10-13 23:15:31 +0900

    creating searchBar

--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -8,19 +8,59 @@ class FeedPage extends StatefulWidget {
 }
 
 class _FeedPageState extends State<FeedPage> {
+  Widget _searchTextField() {
+    return const TextField();
+  }
+
+  Widget _defaultListView() {
+    return ListView.builder(itemBuilder: (context, index) {
+      return const Card();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Feed',
-          style: TextStyle(
-            color: Colors.black,
-            fontFamily: 'Pacifico-Regular',
-          ),
-        ),
-        backgroundColor: Colors.white,
-      ),
-    );
+        appBar: AppBar(
+            title: const Text(
+              'Feed',
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Pacifico-Regular',
+              ),
+            ),
+            backgroundColor: Colors.white,
+            bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(65.0),
+                child: Container(
+                    height: 65.0,
+                    margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Flexible(
+                              child: TextField(
+                                cursorColor: Colors.grey,
+                                decoration: InputDecoration(
+                                  fillColor:
+                                      const Color.fromRGBO(118, 118, 128, 0.12),
+                                  filled: true,
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                      borderSide: BorderSide.none),
+                                  hintText: 'Search',
+                                  prefixIcon: const Icon(Icons.search),
+                                  iconColor:
+                                      const Color.fromRGBO(142, 142, 147, 0),
+                                  hintStyle: const TextStyle(
+                                      color: Colors.grey, fontSize: 17),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )))));
   }
 }
